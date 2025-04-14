cff307760abe89cb05b9a1e7a1d6332f931fcb82
saakshi2002 <saakshishekhar9@gmail.com>
2025-04-14 12:18:23 +0530

    visibility in mobile view hidden

--- frontend/gym-frontend-app/src/components/homepage/Mainsection.tsx
+++ frontend/gym-frontend-app/src/components/homepage/Mainsection.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Header from "../common/Header";
-import Arrow from "../../assets/images/arrow.svg"
-import Underlined from "../../assets/images/fitnessg.svg"
+import Arrow from "../../assets/images/arrow.svg";
+import Underlined from "../../assets/images/fitnessg.svg";
 import DatePickerField from "../common/DatePickerField";
 import dropdownData from "../../assets/JSON/DropdownSelect.json";
 import CoachesData from "../../assets/JSON/Coaches.json";
@@ -39,7 +39,7 @@ const MainSection: React.FC = () => {
 
   const handleSubmit = () => {
     // const formattedDate = filters.date.toISOString().split("T")[0];
-    console.log("Data")
+    console.log("Data");
 
     const results = CoachesData.coaches.filter(
       (session) =>
@@ -59,7 +59,7 @@ const MainSection: React.FC = () => {
     <div>
       <main>
         <Header />
-       
+
         <div className="flex flex-col  lg:text-5xl md:text-4xl sm:text-3xl p-10 gap-4">
           <h1>
             Achieve your{" "}
@@ -71,9 +71,9 @@ const MainSection: React.FC = () => {
             </span>
           </h1>
 
-          <h2 className="flex flex-row gap-4  lg:text-5xl md:text-4xl sm:text-3xl text-center mb-4">
+          <h2 className="flex flex-row gap-4 lg:text-5xl md:text-4xl sm:text-3xl text-center mb-4">
             Find a workout and book today.
-           <img src={Arrow} alt="" />
+            <img src={Arrow} className="hidden sm:inline-block" />
           </h2>
         </div>
 
