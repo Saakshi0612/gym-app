5561315fd418c23ad529ea4382ccc21e0d0b00b7
Shreya Katukuri <shreya_katukuri@epam.com>
2025-04-23 11:31:18 +0530

    lint error in authSlice fixed

--- frontend/gym-frontend-app/src/services/authSlice.tsx
+++ frontend/gym-frontend-app/src/services/authSlice.tsx
@@ -1,7 +1,7 @@
 // src/services/authSlice.ts
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
 import { AuthState, LoginCredentials, RegisterData, User } from '../types';
-import axios from 'axios';
+
 
 interface StoredUser extends User {
   password: string;
